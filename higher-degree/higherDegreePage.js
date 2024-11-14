e5dc6615e160e8f0a13ddfcc1ebcc494ca300f57
Mukund Srinivasan <mukundsrinivasan0@gmail.com>
2024-11-14 13:28:04 +1100

    renamed custom dimension to higher degree
    added curve visualizer
    
    added cards for new modes
    changed colour for cubic
    changed colour for higher deg
    added func for curve vis
    added gamepage
    changed colour for higher deg
    updated images
    
    added arial and bg colour, removed curve vis line between two points
    
    put styles in styles folder
    
    updated readme

--- higher-degree/higherDegreePage.js
+++ higher-degree/higherDegreePage.js
@@ -1,4 +1,4 @@
-import bezierSketch from "./customDimensionToolSketch.js";
+import bezierSketch from "./higherDegreeSketch.js";
 
 window.onload = function() {
     new p5((s) => bezierSketch(s), 'sketchContainer');
